4d73c04f6f15284fdddce59f368ce67140281e98
Aodj05 <aodj05@msn.com>
2022-04-05 21:14:59 -0400

    DOM creation methods

--- src/index.js
+++ src/index.js
@@ -37,7 +37,43 @@ class Model {
 }
 
 class View {
-	constructor(){}
+	constructor(){
+		//root element
+		this.app = this.getElement('#content');
+
+		//Title
+		this.title = this.createElement('h1');
+		this.title.textContent = 'Todos';
+
+		//Form and Buttons
+		this.form = this.createElement('form');
+		this.input = this.createElement('input');
+		this.input.type = 'text';
+		this.input.placeholder = 'Add a todo';
+		this.input.name = 'todo';
+		this.submitButton = this.createElement('button');
+		this.submitButton.textContent = 'Submit';
+
+		//Todo list
+		this.todoList = this.createElement('ul', 'todo-list');
+
+		//add buttons to form
+		this.form.append(this.input, this.submitButton);
+
+		//add title, form, and list
+		this.app.append(this.title, this.form, this.todoList);
+	}
+	//create element with a css class
+	createElement(tag, className) {
+		const element = document.createElement(tag);
+		if(className) element.classList.add(className);
+		return element
+	}
+	//Get element from DOM
+	getElement(select) {
+		const element = document.querySelector(select);
+		return element;
+	}
 }
 
 class Controller {
